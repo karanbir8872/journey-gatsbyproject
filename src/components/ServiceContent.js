7f7f1add3dfd6be0d2eebe47966cae20c070341b
karanbir8872 <wwwwkaranbir786@gmail.com>
2019-10-10 22:03:56 +0530

    minor-fixes

--- src/components/ServiceContent.js
+++ src/components/ServiceContent.js
@@ -9,16 +9,32 @@ const RestContent = () => (
       <div className="container">
         <div className="columns">
           <div className="column">
-            <ServicePhoto img="/images/industry1.jpg" />
+            <ServicePhoto
+              img="/images/services_1.jpg"
+              title="Transportation"
+              subtitle="All transportation cost we bear"
+            />
           </div>
           <div className="column">
-            <ServicePhoto img="/images/industry2.jpg" />
+            <ServicePhoto
+              img="/images/services_2.png"
+              title="Guidence"
+              subtitle="We offer the best guidence for you"
+            />
           </div>
           <div className="column">
-            <ServicePhoto img="/images/industry3.jpg" />
+            <ServicePhoto
+              img="/images/services_3.jpg"
+              title="Accomodation"
+              subtitle="Luxarious and comfortable"
+            />
           </div>
           <div className="column">
-            <ServicePhoto img="/images/industry3.jpg" />
+            <ServicePhoto
+              img="/images/services_4.jpg"
+              title="Discover world"
+              subtitle="Best tour plan for your next tour"
+            />
           </div>
         </div>
       </div>
--- src/components/ServicePhoto.js
+++ src/components/ServicePhoto.js
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 
 const Style = styled.div``;
 const ServicePhoto = props => {
-  const { img } = props;
+  const { img, title, subtitle } = props;
   return (
     <Style>
-      <div className="card">
+      <div className="">
         <div className="card-image">
           <figure className="image is-4by3">
             <img src={img} alt="Placeholder image" />
           </figure>
         </div>
         <div className="card-content">
-          <p className="titl">Transportation</p>
-          <p className="subtitl">All transportation cost we bear</p>
+          <p className="titl is-size-4 line">{title}</p>
+          <p className="subtitl is-size-6 has-text-grey-light">{subtitle}</p>
         </div>
       </div>
     </Style>
