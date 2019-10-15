ff5b245dd50e58e533eed1d8acf2c62b61456738
karanbir8872 <wwwkaranbir786@gmail.com>
2019-10-15 12:38:11 +0530

    few-adding

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
+import React from 'react';
 import styled from 'styled-components';
 
-const SliderStyled = styled(Slider)`
+const SliderStyled = styled.div`
   margin-top: -34rem;
   .colr {
     color: ${props => props.theme.mainBrandColor};
@@ -25,76 +24,65 @@ const SliderStyled = styled(Slider)`
     margin-bottom: 2rem;
   }
 `;
-
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-    return (
+const Slider = () => (
+  <div>
+    <SliderStyled>
       <div>
-        <SliderStyled {...settings}>
-          <div>
-            <section className="section">
-              <div className="container">
-                <div className="columns">
-                  <div className="column" />
-                  <div className="column is-two-fifths">
-                    <div className="card">
-                      <div className="card-content">
-                        <p className="colr">Upcoming Event</p>
-                        <p className="colr1">Maldeva-Asia</p>
-                        <p className="colr2">
-                          Waters makes fish every without firmaments saw
-                          had.Morning air subdue.our.Air every one.Whales grass
-                          in fish Whales Winged.
-                        </p>
-                        <p className="col3">
-                          date:
-                          <span>7-october-2019</span>
-                        </p>
-                        <p className="col3">
-                          cost:
-                          <span>Start from $860</span>
-                        </p>
-                        <p className="col3">
-                          organizer:
-                          <span>Martine Ageny</span>
-                        </p>
-                        <div className="rating">
-                          <p>
-                            Rating:
-                            <span className="icon has-text-warning">
-                              <i className="fas fa-star" />
-                            </span>
-                            <span className="icon has-text-warning">
-                              <i className="fas fa-star" />
-                            </span>
-                            <span className="icon has-text-warning">
-                              <i className="fas fa-star" />
-                            </span>
-                            <span className="icon has-text-warning">
-                              <i className="fas fa-star" />
-                            </span>
-                          </p>
-                        </div>
-                        <a className="button is-medium marg">Plan Details</a>
-                      </div>
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column" />
+              <div className="column is-two-fifths">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="colr">Upcoming Event</p>
+                    <p className="colr1">Maldeva-Asia</p>
+                    <p className="colr2">
+                      Waters makes fish every without firmaments saw had.Morning
+                      air subdue.our.Air every one.Whales grass in fish Whales
+                      Winged.
+                    </p>
+                    <p className="col3">
+                      date:
+                      <span>7-october-2019</span>
+                    </p>
+                    <p className="col3">
+                      cost:
+                      <span>Start from $860</span>
+                    </p>
+                    <p className="col3">
+                      organizer:
+                      <span>Martine Ageny</span>
+                    </p>
+                    <div className="rating">
+                      <p>
+                        Rating:
+                        <span className="icon has-text-warning">
+                          <i className="fas fa-star" />
+                        </span>
+                        <span className="icon has-text-warning">
+                          <i className="fas fa-star" />
+                        </span>
+                        <span className="icon has-text-warning">
+                          <i className="fas fa-star" />
+                        </span>
+                        <span className="icon has-text-warning">
+                          <i className="fas fa-star" />
+                        </span>
+                      </p>
                     </div>
+                    <a className="button is-medium marg">Plan Details</a>
                   </div>
                 </div>
               </div>
-              <div>
-                <h3>2</h3>
-              </div>
-            </section>
+            </div>
           </div>
-        </SliderStyled>
+          <div>
+            <h3>2</h3>
+          </div>
+        </section>
       </div>
-    );
-  }
-}
+    </SliderStyled>
+  </div>
+);
+export default Slider;
--- src/html.js
+++ src/html.js
@@ -14,18 +14,6 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-
-          <link
-            rel="stylesheet"
-            type="text/css"
-            charset="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"
