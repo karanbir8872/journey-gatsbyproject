b8dc64e1f3b22e51b5a0004a82445524c1a61fcb
karanbir8872 <wwwkaranbir786@gmail.com>
2019-10-15 11:25:56 +0530

    add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Wrapper = styled.div`
   color: white;
   .marg {
     margin-top: 2rem;
+    margin-bottom: 2rem;
   }
   ul {
     line-height: 1.5;
@@ -77,9 +78,29 @@ const Footer = () => (
                 <i className="fas fa-envelope" />
               </span>
             </div>
-            <p>jhjdhhsdjsh</p>
+            <p className="">
+              Subscribe our newsletter to get update news and offers
+            </p>
           </div>
-          <div className="column">CONTACT US</div>
+          <div className="column">
+            <p>CONTACT US</p>
+            <p className="marg">
+              4156, New garden, New York, USA +880 362 352 783
+            </p>
+            <p className="marg">contact@martine.com</p>
+            <span className="icon has-text-white">
+              <i className="fab fa-facebook" />
+            </span>
+            <span className="icon has-text-white">
+              <i className="fab fa-twitter-square" />
+            </span>
+            <span className="icon has-text-white">
+              <i className="fab fa-pinterest-square" />
+            </span>
+            <span className="icon has-text-white">
+              <i className="fab fa-instagram" />
+            </span>
+          </div>
         </div>
       </div>
     </section>
